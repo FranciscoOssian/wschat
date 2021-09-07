f509f52cdfc2dc25fd203b6d60b701850ab0c144
FranciscoOssian <ossian.neto@gmail.com>
2021-09-07 08:18:56 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const path = require('path');
+
+const express = require('express');
+const app = express();
+
+const server = require('http').createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST", "DELETE"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  },
+  maxHttpBufferSize: 1e8
+});
+
+port = process.env.PORT | 8000
+
+//used only to run the example site ---------------------------
+app.use( express.static( path.join( __dirname, 'public' ) ) )
+app.set( 'views', path.join( __dirname, 'public' ) )
+app.engine( 'html', require('ejs').renderFile )
+app.set( 'view engine', 'html' )
+app.use('/', (req, res) => {
+  res.render('index.html')
+})
+//-------------------------------------------------------------
+
+io.on('connection', (socket) => {
+  
+  console.log(`a user connected ${socket.id}`);
+
+  socket.on('message', message => {
+    console.log(`message recived:`)
+    console.log(`${message.author_uid_sender} falou ${message.content} para ${message.author_uid_to}`)
+    io.sockets.to(message.author_uid_to).emit('message', message);
+  })
+
+});
+
+server.listen(port, () => {
+  console.log(`listening on *:${port} port`);
+});
---
+++ public/js/main.js
@@ -0,0 +1,84 @@
+const { useState, useEffect } = React
+
+const socket = io(`http://192.168.0.51:8000`)
+
+let messagesTemp = []
+
+const App = () => {
+
+  const [reciver, setReciver] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const [myToken, setMyToken] = useState('')
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('your id is', socket.id)
+      setMyToken(socket.id)
+    })
+
+    socket.on('message', message => {
+      messagesTemp.push(message)
+      setMessages([...messagesTemp])
+    })
+
+  }, [])
+
+  const onSubmitHandler = (e) => {
+    e.preventDefault()
+    socket.emit('message',
+      {
+        content: message,
+        author_uid_sender: socket.id,
+        author_uid_to: reciver
+      }
+    )
+  }
+
+  return (
+    <div>
+
+      <h1>chat</h1>
+      <h2>your token is {myToken}</h2>
+
+      {
+        messages.map( message => {
+          return(
+            <div>{message.content}</div>
+          )
+        } )
+      }
+
+      <form>
+
+        <label
+          for="token_to"
+        >Token of the person</label>
+        <input
+          type="text"
+          id="token_to"
+          name="token_to"
+          placeholder="token person"
+          onChange={(e) => setReciver(e.target.value)}
+        />
+
+        <label
+          for="message"
+        >Message</label>
+        <input
+          type="text"
+          id="message"
+          name="message"
+          placeholder="message"
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <input type="submit" value="Submit" onClick={(e) => onSubmitHandler(e)}/>
+
+      </form>
+
+    </div>
+  )
+}
+
+
+ReactDOM.render(<App />, document.querySelector('#root'));
